96c81fa01acebb54575584a8b61a3091b36dce68
JDonavan <donavan.stanley@gmail.com>
2015-08-30 12:59:32 -0400

    Add a helper to the module for spinning up a server from address and port.  Update binaries to use it.

--- lib/arkcon.rb
+++ lib/arkcon.rb
@@ -3,4 +3,10 @@ require 'arkcon/ark_server'
 require 'arkcon/ark_monitor'
 module Arkcon
   # Your code goes here...
+  def self.connect(address, port, password)
+    socket = RCONSocket.new address, port
+    server = Arkcon::ArkServer.new socket
+    server.authenticate password
+    server
+  end
 end
